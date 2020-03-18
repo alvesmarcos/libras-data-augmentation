302368faa312a96cf59a14554425218a85e11673
Renan-Goes <36930305+Renan-Goes@users.noreply.github.com>
2020-03-18 13:01:33 -0300

    Update augmentation_negation.py

--- augmentation_negation.py
+++ augmentation_negation.py
@@ -1,46 +1,61 @@
 import re
 import pandas as pd
 import json
+import itertools
 
-def augmentation_negation(list_negation, re_fetch, match_negation, line):
-    lista = [(line[0], line[1])]
+def augmentation_negation(list_negation, re_fetch, line):
+    lista = []
+
     if re.search(re_fetch, line[0]) != None and re.search(re_fetch, line[1]) != None:
-        for index, word_neg in enumerate(list_negation):
-            if index == 20:
-                break
+        possibilities_negation = re.findall(re_fetch, line[0])
+        number_of_words = len(possibilities_negation)
+        if number_of_words == 1:
+            lista.append(line)
+            lista.append((re.sub(re_fetch, r'NÃO_\1', line[0]), re.sub(re_fetch, r'NÃO_\1', line[1])))
+        else:
+            words = []
+            for index, word in enumerate(possibilities_negation):
+                if index == number_of_words:
+                    break
+                words.append(r'\b' + word[0] + r'\b')
+                possibilities_negation[index] = [word[0]]
+                possibilities_negation[index].append('NÃO_' + word[0])
 
-            print(f'Frase: {line[0]} --- frase(gi): {line[1]}')
-            word_neg_split = word_neg[0].split('_')[1]
-            fetch_word = match_negation["\\b(" + word_neg_split + ")\\b"]
-            print(f'fetch_word = {word_neg_split} --- match_negation[fetch_word] = {fetch_word}')
-            lista.append((re.sub(re_fetch, fetch_word, line[0]), re.sub(re_fetch, fetch_word, line[1])))
+            permutations = itertools.product(*possibilities_negation)
+            indexes_of_word = itertools.cycle([0, 1])
+            for word in permutations:
+                sentence_gr = line[0]
+                sentence_gi = line[1]
+                for i in range(number_of_words):
+                    sentence_gr = re.sub(words[i], word[i], sentence_gr)
+                    sentence_gi = re.sub(words[i], word[i], sentence_gi)
+                    
+                lista.append((sentence_gr, sentence_gi))
+                
+    if lista != []:
+        return lista
+    else:
+        return line
 
-    return lista
-
-df_negation = pd.read_csv('negacao.csv')
+df_negation = pd.read_csv('negacao.csv', header=None)
 
 list_negation = df_negation.values.tolist()
 
-fetching_expression = ""
-match_negation = {}
+fetching_expression = "("
 
 for neg in list_negation:
     word = neg[0].split('_')[1]
-    fetching_expression += "\\b(" + word.upper() + ")\\b|"
-    match_negation[r'\b(' + word.upper() + r')\b'] = "(" + neg[0].upper() + ")"
+    fetching_expression += r"(\b" + word.upper() + r"\b)|"
 
-#print(json.dumps(match_negation, indent=4))
-
-fetching_expression[:-1]
+fetching_expression = fetching_expression[:-1]
+fetching_expression += ")"
 re_fetch = r"" + fetching_expression
 
-#print(re_fetch)
-
-teste_frases = [('WEINTRAUB CONVERSAR JAIR BOLSONARO', 'WEINTRAUB&FAMOSO CONVERSAR JAIR_BOLSONARO&FAMOSO'), ('WEINTRAUB SAIR', 'WEINTRAUB&FAMOSO SAIR'),
+teste_frases = [('WEINTRAUB CONVIDAR JAIR BOLSONARO IR CASA', 'WEINTRAUB&FAMOSO CONVIDAR JAIR_BOLSONARO&FAMOSO IR CASA'), ('WEINTRAUB SAIR', 'WEINTRAUB&FAMOSO SAIR'),
                 ('JOHN LENNON IR CASA', 'JOHN_LENNON&FAMOSO IR CASA'), ('HARRY POTTER CONVERSAR HITLER', 'HARRY_POTTER&FAMOSO CONVERSAR HITLER&FAMOSO'),
-                ('OPA', 'OPA')]
+                ('OPA', 'OPA'), ('EU NOTAR ALI NEGAR BOM', 'EU NOTAR ALI NEGAR BOM')]
                 
 
 for i in teste_frases:
-    print(augmentation_negation(list_negation, re_fetch, match_negation, i))
+    print(augmentation_negation(list_negation, re_fetch, i), '\n')
 
